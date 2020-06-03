30a2727205d7d2a6d87f59dcdc853ce05758432b
Nikunj Dalsaniya <nikunj5390.dalsaniya@gmail.com>
2020-06-03 22:57:59 +0530

    api server url updated

--- clientServer/src/apis/streams.js
+++ clientServer/src/apis/streams.js
@@ -4,5 +4,5 @@ export default axios.create({
   baseURL:
     process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
-      : "",
+      : "https://streams-api-server.herokuapp.com",
 });
